9dac39144ba63339de4b21e3e01e25a6986ddc9b
Trejon-Hughes <trejonhughes15@gmail.com>
2021-04-12 18:47:13 -0400

    Added funcionality for posting and retrieving posts

--- SocialMediaAPI.Data/Post.cs
+++ SocialMediaAPI.Data/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace SocialMediaAPI.Data
 {
     public class Post
     {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        //public virtual List<Comment> Comments { get; set; }
+        //public virtual List<Like> Likes { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
     }
 }
---
+++ SocialMediaAPI.Web/Controllers/PostController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMediaAPI.Model;
+using SocialMediaAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMediaAPI.Web.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        private PostService CreatePostService()
+        {
+            var authorId = Guid.Parse(User.Identity.GetUserId());
+            var PostService = new PostService(authorId);
+            return PostService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
