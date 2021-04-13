4144b3ceb581467fee19777aca2d434df76f28a9
kevstockbridge <stockbridge.kevin@gmail.com>
2021-04-13 09:33:12 -0400

    Kev Push 1

---
+++ SocialMediaAPI.Data/ReplyCreate.cs
@@ -0,0 +1,23 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class ReplyCreate
+    {
+        [Required]
+        [MaxLength(8000)]
+        public string Text { get; set; }
+        [Required]
+        public User Author { get; set; }
+        [Required]
+        public Post CommentPost { get; set; }
+        [Required]
+        public Comment ReplyComment { get; set; }
+    }
+}
---
+++ SocialMediaAPI.Data/ReplyGet.cs
@@ -0,0 +1,14 @@
+﻿using SocialMedia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class ReplyGet
+    {
+        public Comment ReplyComment { get; set; }
+    }
+}
