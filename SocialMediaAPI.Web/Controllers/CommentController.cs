04d5c335cc01daacbc1b8d6b1fec6db4ed66fe02
ekkaiden <2003kaiden@gmail.com>
2021-04-14 09:42:38 -0400

    kaiden

---
+++ SocialMediaAPI.Web/Controllers/CommentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMediaAPI.Model;
+using SocialMediaAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace SocialMediaAPI.Web.Controllers
+{
+    public class CommentController : ApiController
+    {
+        private CommentServices CreateCommentService()
+        {
+            var authorId = Guid.Parse(User.Identity.GetUserId());
+            var CommentService = new CommentServices(authorId);
+            return CommentService;
+        }
+        public IHttpActionResult Comment(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCommentService();
+
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get(int postId)
+        {
+            CommentServices commentService = CreateCommentService();
+            var comment = commentService.GetCommentByPostId(postId);
+            return Ok(comment);
+        }
+    }
+}
